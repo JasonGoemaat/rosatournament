fea71e9458ab369af6c81c448effe2144e5c9423
Jason Goemaat <jason.goemaat@gmail.com>
2021-10-31 19:00:06 -0500

    Add facebook comments plugin

--- src/app/tournaments/tournaments/tournaments.component.ts
+++ src/app/tournaments/tournaments/tournaments.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router, RouterEvent } from '@angular/router';
 import { Observable } from 'rxjs/internal/Observable';
-import { tap } from 'rxjs/operators';
+import { filter, map, startWith, tap } from 'rxjs/operators';
 import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 import { TournamentService, MyRouteData } from 'src/app/services/tournament.service';
@@ -14,23 +14,31 @@ import { tournamentReset } from 'src/app/models/reset';
 })
 export class TournamentsComponent implements OnInit, OnDestroy {
   tournamentId: string = '';
-  data$: Observable<MyRouteData>
-  paramMap? : Subscription;
+  data$: Observable<MyRouteData>;
+  location$: Observable<string>;
+  paramMap?: Subscription;
 
   constructor(
     public route: ActivatedRoute,
     public service: TournamentService,
     public authService: AuthService,
     public router: Router,
-    ) {
+  ) {
     (window as any).cTournaments = this;
     this.data$ = this.service.getForParams(this.route.paramMap).pipe(tap(x => {
       console.log('getForParams returned:', x);
       (window as any).t = x;
     }));
+    this.location$ = this.router.events
+      .pipe(filter(e => e instanceof NavigationEnd), startWith(this.router))
+      .pipe(tap(x => console.log('%crouter.events:', 'padding: 5px; border-radius: 5px; color: white; background-color: black; font-weight: bold', x)))
+      //.pipe(tap(e => e.url))
+      .pipe(tap(l => console.log('location:', l)))
+      .pipe(map(x => `https://rosatournament.web.app${location.pathname}`))
   }
 
   ngOnInit(): void {
+    this.location$.subscribe(x => console.log('Well, got x!', x));
   }
 
   ngOnDestroy(): void {
